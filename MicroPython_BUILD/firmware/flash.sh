06ac0cf11ae4761cb9717a929d03e601c75bd046
Alex Vrubel <alex.vrubel@gmail.com>
2018-11-18 20:38:07 +0300

    Added prebuilt firmware for indicator.fun

--- MicroPython_BUILD/firmware/flash.sh
+++ MicroPython_BUILD/firmware/flash.sh
@@ -38,4 +38,4 @@ get_arguments() {
 
 get_arguments "$@"
 
-esptool.py --chip esp32 --port ${FLASH_COMPORT} --baud ${FLASH_BDRATE} --before default_reset --after no_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader/bootloader.bin 0xf000 phy_init_data.bin 0x10000 MicroPython.bin 0x8000 partitions_mpy.bin
+esptool.py --chip esp32 --port ${FLASH_COMPORT} --baud ${FLASH_BDRATE} --before default_reset --after no_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader/bootloader.bin 0xf000 phy_init_data.bin 0x10000 MicroPython.bin 0x8000 partitions_mpy.bin 0x330000 spiffs_image.img
